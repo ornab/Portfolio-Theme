4b2ee6ff4016f355eb523c6fb175d559cd984219
ornab <hzamil17@gmail.com>
2017-08-01 22:36:02 +0600

    Modified

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php get_header();?>
+
+<?php get_template_part('template/slider');?>
+<?php get_template_part('template/about-logic');?>
+<?php get_template_part('template/featured-work');?>
+<?php get_template_part('template/video');?>
+<?php get_template_part('template/service');?>
+<?php get_template_part('template/accordian');?>
+<?php get_template_part('template/team');?>
+<?php get_template_part('template/latest-project');?>
+<?php get_template_part('template/contact');?>
+
+
+<?php get_footer();?>
+
+
